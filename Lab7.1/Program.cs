96018472931f2400a0c0874f85586a5413cc4a7f
stasserebrennikov59@gmail.com <stasserebrennikov59@gmail.com>
2021-12-20 00:53:24 +0300

    Добавьте файлы проекта.

---
+++ Lab7.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7._1
+{
+    internal class Program
+    {
+        public struct Triangle
+        {
+            public double a,b,c;
+            public double Square ()
+            {
+                double p = (a + b + c) / 2;
+                double s = Math.Sqrt (p*(p-a)*(p-b)*(p-c));
+                return s;       
+            }
+        }
+        static void Main(string[] args)
+        {
+            Triangle [] triangles = new Triangle[2];
+            for(int i = 0; i<triangles.Length; i++)
+            {
+                Console.WriteLine("Ввести значание длин (a,b,c) сторон треугольника {0}", i+1);
+                triangles[i].a = double.Parse(Console.ReadLine());
+                triangles[i].b = double.Parse(Console.ReadLine());
+                triangles[i].c = double.Parse(Console.ReadLine());
+            }
+            for (int i = 0;i< triangles.Length; i++)
+            {
+                Console.WriteLine("Площадь {0} - го треугольника = {1}", i+1, triangles[i].Square());
+            }
+            int maxSquaredTriIdx = 0;
+            for (int i = 0; i < triangles.Length; i++)
+            {
+                if (triangles[i].Square() > triangles[maxSquaredTriIdx].Square())
+                    maxSquaredTriIdx = i;   
+            }
+            Console.WriteLine("Макс площадь у {0}-го треугольника", maxSquaredTriIdx+1);
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ Lab_7.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+            x = y = z;
+            int V, S;
+            GetParam (x,z,y, out V, out S);
+            Console.WriteLine(V);
+            Console.WriteLine(S);
+            Console.ReadKey();
+        }
+        static void GetParam(int x,int y,int z, out int V, out int S)
+        {
+            V = x*y*z; 
+            S = 6*y*z;    
+        }
+        
+
+    }
+}
